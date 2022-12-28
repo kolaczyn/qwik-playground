42958c393cf84c4955f27eb11e7f0fdbe61f4658
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-12-28 20:06:58 +0100

    WIP add component test with vitest

---
+++ src/components/todo-input.test.tsx
@@ -0,0 +1,9 @@
+// import { createDOM } from '@builder.io/qwik/testing';
+import { test, vi } from "vitest";
+// import { TodoInput } from './todo-input';
+
+test("todo-input", async () => {
+  //   const {screen, render } = await createDOM()
+  //   const handleAddTodo = vi.mock()
+  //   await render(<TodoInput handleAddTodo$={() => handleAddTodo()} />)
+});
